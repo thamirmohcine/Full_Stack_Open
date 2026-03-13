806bf3df48020eaa245141a944b85a7f428b1106
Mouhcine Thamir <mouhcinethamir@gmail.com>
2026-03-13 22:35:21 +0000

    feat: finish  Course information step 10 (2.5)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,19 +1,4 @@
-
-const Part = ({obj}) => {
-  return (
-    <p>{obj.name} {obj.exercises}</p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <h2>{course.name}</h2>
-      {course.parts.map((part) => <Part key={part.id} obj={part} />)}
-      <h3>Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</h3>
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
