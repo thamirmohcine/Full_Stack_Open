9f4c25acf42d890efa835f3c88467b15efbbe8e8
thamirmohcine <thamirmohsine@gmail.com>
2026-03-17 12:35:52 +0000

    feat: Phonebook backend step 3 (3.3)

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,15 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(p => p.id === id);
+    if (person)
+        response.send(person);
+    response.status(404).end();
+
+})
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`)
