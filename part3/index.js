06833735843cde7a4a1d8417b6768bb1fa2f3879
thamirmohcine <thamirmohsine@gmail.com>
2026-03-17 15:22:00 +0000

    feat: Phonebook backend step 8 (3.8)

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,15 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json());
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body);
+    }
+})
+
+
 let persons = [{
     "id": "1",
     "name": "Arto Hellas",
